360bfc7be0e00f836339ac72dd0eced6fd8b0f3d
chmaha <reaclassical@gmail.com>
2023-01-06 15:34:24 +0000

    Move to resource base folder and plugins for Linux and MacOS script

--- Resource Folders/ReaClassical_macOS.sh
+++ Resource Folders/ReaClassical_macOS.sh
@@ -14,24 +14,26 @@ else
     echo "Using universal dmg file..."
 fi
 
-echo "Downloading REAPER from reaper.fm"
+echo "Downloading REAPER $pkgver from reaper.fm"
 sleep 2
 curl https://reaper.fm/files/${pkgver::1}.x/reaper${pkgver//.}_$dmgtype.dmg -L -o reaper.dmg
 echo "Converting and mounting DMG..."
 sleep 2
 hdiutil convert -quiet reaper.dmg -format UDTO -o reaper_temp
 hdiutil attach -quiet -nobrowse -noverify -noautoopen -mountpoint ./reaper_temp  reaper_temp.cdr
-echo "Downloading ReaClassical resource folder for MacOS"
+echo "Downloading ReaClassical resource folder base and userplugins for MacOS"
 sleep 2
-curl https://github.com/chmaha/ReaClassical/raw/main/Resource%20Folders/MacOS-$arch.zip -o resource_folder.zip -L
+curl -O https://github.com/chmaha/ReaClassical/raw/main/Resource%20Folders/Resource_Folder_Base.zip -L
+curl -O https://github.com/chmaha/ReaClassical/raw/main/Resource%20Folders/UserPlugins/UP_MacOS-$arch.zip -L
 echo "Extracting files into ReaClassical folder..."
 sleep 2
-unzip -q resource_folder.zip -d ReaClassical/
+unzip -q Resource_Folder_Base.zip -d ReaClassical/
+unzip -q UP_MacOS-$arch.zip -d ReaClassical/UserPlugins/
 echo "Copying REAPER.app into ReaClassical folder..."
 sleep 2
 cp -R reaper_temp/REAPER.app ReaClassical/
 echo "Unmounting image and deleting temporary files..."
 sleep 2
 hdiutil detach reaper_temp
-rm reaper.dmg reaper_temp.cdr resource_folder.zip
+rm reaper.dmg reaper_temp.cdr Resource_Folder_Base.zip UP_MacOS-$arch.zip
 echo "Portable ReaClassical Installation complete!"
