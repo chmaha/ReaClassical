8e789edbc9a50a92f41190e9dd8248fb104fee08
chmaha <reaclassical@gmail.com>
2023-01-25 13:23:00 +0000

    Add help system

---
+++ ReaClassical/ReaClassical_Help.lua
@@ -0,0 +1,47 @@
+--[[
+@noindex
+
+This file is a part of "ReaClassical" package.
+See "ReaClassical.lua" for more information.
+
+Copyright (C) 2022 chmaha
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+]]
+
+local r = reaper
+local file_exists, retrieve
+local resource_path = r.GetResourcePath()
+local relative_path = "Scripts/chmaha Scripts/PDF_Guide/ReaClassical_PDF_Guide.pdf"
+local pdf = resource_path .. "/" .. relative_path
+
+function main()
+  local bool = file_exists(pdf)
+  if bool == true then
+    reaper.CF_ShellExecute(pdf)
+  else
+    r.ShowMessageBox("Install ReaClassical PDF guide via ReaPack first!", "ReaClassical PDF Guide not found!", 0)
+  end
+end
+
+function file_exists(name)
+   local exists = false
+   local file=io.open(name,"r")
+   if file ~= nil then 
+    io.close(file)
+    exists = true
+   end
+   return exists
+end
+
+main()
+ 
