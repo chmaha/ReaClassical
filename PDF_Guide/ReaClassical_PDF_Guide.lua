6f03f1e371ca41825d33afef710dc4bda080f0d4
chmaha <reaclassical@gmail.com>
2023-02-05 17:18:20 +0000

    Bump to v23.06

--- PDF_Guide/ReaClassical_PDF_Guide.lua
+++ PDF_Guide/ReaClassical_PDF_Guide.lua
@@ -1,4 +1,4 @@
 @description ReaClassical PDF Guide
 @author chmaha
-@version 23.05
+@version 23.06
 @provides ReaClassical_PDF_Guide.pdf https://github.com/chmaha/ReaClassical/raw/main/ReaClassical%20User%20Guide.pdf
