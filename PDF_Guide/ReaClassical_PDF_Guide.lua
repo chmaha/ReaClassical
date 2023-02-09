49e64ce7b559935ffba6c5d3f7c4010a5c0e55cf
chmaha <reaclassical@gmail.com>
2023-02-09 12:50:01 +0000

    Complete appendix D

--- PDF_Guide/ReaClassical_PDF_Guide.lua
+++ PDF_Guide/ReaClassical_PDF_Guide.lua
@@ -1,4 +1,4 @@
 @description ReaClassical PDF Guide
 @author chmaha
-@version 23.06
+@version 23.07
 @provides ReaClassical_PDF_Guide.pdf https://github.com/chmaha/ReaClassical/raw/main/ReaClassical%20User%20Guide.pdf
