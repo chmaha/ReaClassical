909c92bd503b51b55e331b95adf8bb36c55356da
chmaha <reaclassical@gmail.com>
2023-02-12 22:06:15 +0000

    Use local PDF guide in lua

--- PDF_Guide/ReaClassical_PDF_Guide.lua
+++ PDF_Guide/ReaClassical_PDF_Guide.lua
@@ -2,5 +2,4 @@
 @author chmaha
 @version 23.11
 @provides 
-  ReaClassical_PDF_Guide.pdf https://github.com/chmaha/ReaClassical/raw/main/PDF_Guide/ReaClassical%20User%20Guide.pdf
-  [nomain] .
+   ReaClassical User Guide.pdf > ReaClassical_PDF_Guide.pdf
