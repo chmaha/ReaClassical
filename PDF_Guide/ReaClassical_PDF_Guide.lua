b1a38e7ec9597323e47236b394646f83636a2a32
chmaha <reaclassical@gmail.com>
2023-02-12 19:27:13 +0000

    Move around appendices and small clarifications

--- PDF_Guide/ReaClassical_PDF_Guide.lua
+++ PDF_Guide/ReaClassical_PDF_Guide.lua
@@ -1,6 +1,6 @@
 @description ReaClassical PDF Guide
 @author chmaha
-@version 23.10
+@version 23.11
 @provides 
   ReaClassical_PDF_Guide.pdf https://github.com/chmaha/ReaClassical/raw/main/PDF_Guide/ReaClassical%20User%20Guide.pdf
   [nomain] .
