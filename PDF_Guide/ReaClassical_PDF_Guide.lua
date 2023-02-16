8a17fff1a599a9551aba5c61e18e0d53a5b5f9b0
chmaha <reaclassical@gmail.com>
2023-02-16 10:36:14 +0000

    Add new plugin info

--- PDF_Guide/ReaClassical_PDF_Guide.lua
+++ PDF_Guide/ReaClassical_PDF_Guide.lua
@@ -1,6 +1,8 @@
 @description ReaClassical PDF Guide
 @author chmaha
-@version 23.12
+@version 230216
+@changelog
+  Disconnect from ReaClassical versioning (use same versioning as Resource Folder Base)
 @provides 
    ReaClassical User Guide.pdf > ReaClassical_PDF_Guide.pdf
    [nomain] .
