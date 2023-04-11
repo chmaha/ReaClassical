c7bdd91c0e3495dd6ed799b31635b01a5055c152
chmaha <reaclassical@gmail.com>
2023-04-11 13:09:24 +0100

    Add full changelog

--- ReaClassical/ReaClassical.lua
+++ ReaClassical/ReaClassical.lua
@@ -2,6 +2,31 @@
 @author chmaha
 @version 23.19rc1
 @changelog
+  Whole Project View: Switch back to original vertical zoom function for updated SWS
+  Aux and submix: Allow for creation of aux and submix (@ track prefix) that stay visible
+  Sync FX and routing: All child routing is now also copied from destination to source groups
+  Classical Take Record: Add guard clause if no folder/track selected
+  New Script: Add Aux/Submix @ track with color #4C9165 to end of tracklist
+  Create Source Groups: Don't apply grouping for aux/submix tracks
+  Various Scripts: Use Solo in Place (SIP) to allow for unmuted sends to aux/submixes
+  Various scripts: Change color of @ aux/submix tracks if created manually
+  New Script: Reposition Tracks
+  Reposition Tracks: Now works with both prepared takes grouping (T) and newer media/razor editing grouping
+  Create CD markers: If run save a key/value pair and change opening dialog to yes/no options.
+  Reposition Tracks: Use key/value pair to automatically re-run Create CD Markers after repositioning
+  Reposition Tracks: Add guard clauses to deal with zero track count, zero media items or presence of empty items
+  Reposition Tracks: Rename to Reposition Album Tracks
+  Add Aux/Submix @ track: Only allow use if a folder exists already
+  Prepare Takes: Change messagebox to yes/no if edits present
+  Prepare Takes: Improve wording of message when offering to remove item take names when edits present
+  Prepare Takes: Always ask if user wants to remove item take names
+  mpl Markers to CUE: Complete revision of code including correcting a cue file specification error, added width to dialog box, removal of external dependencies and simplifying the main function to use separated functions
+  Added Markers to CUE script to ReaClassical repository with permission
+  Remove JS API from UserPlugins
+  Create Source Groups: Allow for creating lead/follow groups with existing manually-created folders
+  Create CD Markers: Create pregap markers by prefixing take name with "!"
+  Markers to CUE: Match CUE file name with audio filename given in dialog
+  Markers to CUE: Save user-inputted metadata and recall on next run
   PDF Guide: Switch to sans serif font and add typsetting info on first page
 @metapackage
 @provides
