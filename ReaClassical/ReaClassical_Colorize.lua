beb1e452dd5450144a52e9c10305ea8cdf925689
chmaha <reaclassical@gmail.com>
2023-05-04 16:06:25 +0100

    New function: Colorize

---
+++ ReaClassical/ReaClassical_Colorize.lua
@@ -0,0 +1,28 @@
+--[[
+@noindex
+
+This file is a part of "ReaClassical" package.
+See "ReaClassical.lua" for more information.
+
+Copyright (C) 2023 chmaha
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+]]
+
+for key in pairs(reaper) do _G[key] = reaper[key] end
+
+function Main()
+  Main_OnCommand(40034,0) -- select all in group
+  Main_OnCommand(40706,0) -- set to random color
+end
+
+Main()
