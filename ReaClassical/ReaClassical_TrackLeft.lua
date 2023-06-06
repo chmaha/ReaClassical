2281916c4e26b4dbfd511748d5e22cbbfd9638b5
chmaha <reaclassical@gmail.com>
2023-06-06 10:03:43 +0100

    NO-OP: Beautify code part 3

--- ReaClassical/ReaClassical_TrackLeft.lua
+++ ReaClassical/ReaClassical_TrackLeft.lua
@@ -17,31 +17,30 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 ]]
-local r = reaper
-local takename_check, check_position, get_track_info, select_CD_track_items, calc_postgap, select_and_cut, paste, go_to_previous, shift, get_grouped_items
----------------------------------------------------------------------------------------
-function Main()
-    r.Undo_BeginBlock()
+
+for key in pairs(reaper) do _G[key] = reaper[key] end
+
+---------------------------------------------------------------------
+
+function main()
+    Undo_BeginBlock()
 
     local take_name, selected_item = takename_check()
     if take_name == -1 or take_name == "" then
-        r.ShowMessageBox('Please select an item that starts a CD track', "Select CD track start", 0)
+        ShowMessageBox('Please select an item that starts a CD track', "Select CD track start", 0)
         return
     end
 
     local ret, item_number = check_position(selected_item)
     if ret then
-        r.ShowMessageBox('The selected track is already in first position', "Select CD track start", 0)
+        ShowMessageBox('The selected track is already in first position', "Select CD track start", 0)
         return
     end
 
     local first_track, num_of_items = get_track_info()
-
     local count = select_CD_track_items(item_number, num_of_items, first_track)
-    
-    
     local new_track_item, postgap = calc_postgap(count, num_of_items, first_track, selected_item)
-    
+
     select_and_cut()
 
     -- shift all future tracks back length of selected track postgap
@@ -49,108 +48,114 @@ function Main()
         shift(first_track, new_track_item, postgap, 0, "left")
     end
 
-
     go_to_previous(item_number, first_track)
-
     paste()
 
-    selected_item = r.GetSelectedMediaItem(0,0)
+    selected_item = GetSelectedMediaItem(0, 0)
 
     -- shift forward all future tracks the length of track postgap
     shift(first_track, selected_item, postgap, count, "right")
 
-    r.Main_OnCommand(40769,0) -- unselect all
-    r.SetMediaItemSelected(selected_item, true)
+    Main_OnCommand(40769, 0) -- unselect all
+    SetMediaItemSelected(selected_item, true)
 
-    r.Undo_EndBlock("Move Track Left", -1)
+    Undo_EndBlock("Move Track Left", -1)
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function takename_check()
-    local item = r.GetSelectedMediaItem(0, 0)
+    local item = GetSelectedMediaItem(0, 0)
     if item then
-      local take = r.GetActiveTake(item) 
-      local _, take_name = r.GetSetMediaItemTakeInfo_String(take, "P_NAME", "", false)
-      return take_name, item
+        local take = GetActiveTake(item)
+        local _, take_name = GetSetMediaItemTakeInfo_String(take, "P_NAME", "", false)
+        return take_name, item
     else
-      return -1
+        return -1
     end
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function check_position(item)
-    local item_number = r.GetMediaItemInfo_Value(item, "IP_ITEMNUMBER")
+    local item_number = GetMediaItemInfo_Value(item, "IP_ITEMNUMBER")
     return item_number == 0, item_number
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function get_track_info()
-    local first_track = r.GetTrack(0, 0)
-    return first_track, r.GetTrackNumMediaItems(first_track)
+    local first_track = GetTrack(0, 0)
+    return first_track, GetTrackNumMediaItems(first_track)
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function paste()
-    r.Main_OnCommand(42398, 0)
+    Main_OnCommand(42398, 0)
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function select_and_cut()
-    r.Main_OnCommand(40034, 0) -- select all items in groups
-    r.Main_OnCommand(40699, 0) -- paste items
+    Main_OnCommand(40034, 0) -- select all items in groups
+    Main_OnCommand(40699, 0) -- paste items
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function go_to_previous(item_number, track)
     for i = item_number - 1, 0, -1 do
-        local prev_item = r.GetTrackMediaItem(track, i)
-        local take = r.GetActiveTake(prev_item)
-        local _, take_name = r.GetSetMediaItemTakeInfo_String(take, "P_NAME", "", false)
+        local prev_item = GetTrackMediaItem(track, i)
+        local take = GetActiveTake(prev_item)
+        local _, take_name = GetSetMediaItemTakeInfo_String(take, "P_NAME", "", false)
         if take_name ~= "" then
-            local prev_item_pos = r.GetMediaItemInfo_Value(prev_item, "D_POSITION")
-            r.SetEditCurPos(prev_item_pos, false, false)
+            local prev_item_pos = GetMediaItemInfo_Value(prev_item, "D_POSITION")
+            SetEditCurPos(prev_item_pos, false, false)
             break
         end
     end
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function shift(track, item, shift_amount, items_in_track, direction)
-    local num_of_items = r.GetTrackNumMediaItems(track)
-    local item_number = r.GetMediaItemInfo_Value(item, "IP_ITEMNUMBER")
+    local num_of_items = GetTrackNumMediaItems(track)
+    local item_number = GetMediaItemInfo_Value(item, "IP_ITEMNUMBER")
     local items_to_move = {}
     if direction == "right" then
         item_number = item_number + items_in_track + 1
         shift_amount = -shift_amount
     end
-    r.Main_OnCommand(40289, 0) -- unselect all items
+    Main_OnCommand(40289, 0) -- unselect all items
     for i = item_number, num_of_items - 1, 1 do
-        local item = r.GetTrackMediaItem(track, i)
-        r.SetMediaItemSelected(item, true)
-        r.Main_OnCommand(40034, 0) -- Item grouping: Select all items in groups
+        local item = GetTrackMediaItem(track, i)
+        SetMediaItemSelected(item, true)
+        Main_OnCommand(40034, 0) -- Item grouping: Select all items in groups
     end
-    local selected_item_count = r.CountSelectedMediaItems(0)
+    local selected_item_count = CountSelectedMediaItems(0)
     for i = 0, selected_item_count - 1 do
-         items_to_move[#items_to_move + 1] = r.GetSelectedMediaItem(0, i)
+        items_to_move[#items_to_move + 1] = GetSelectedMediaItem(0, i)
     end
-    r.Main_OnCommand(40289, 0) -- unselect all items
+    Main_OnCommand(40289, 0) -- unselect all items
     for _, v in pairs(items_to_move) do
-         local item_pos = r.GetMediaItemInfo_Value(v, "D_POSITION")
-         r.SetMediaItemInfo_Value(v, "D_POSITION", item_pos - shift_amount)
+        local item_pos = GetMediaItemInfo_Value(v, "D_POSITION")
+        SetMediaItemInfo_Value(v, "D_POSITION", item_pos - shift_amount)
     end
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function select_CD_track_items(item_number, num_of_items, track)
     local count = 0
     local added_item
     if item_number ~= num_of_items - 1 then
         for i = item_number + 1, num_of_items - 1, 1 do
-            added_item = r.GetTrackMediaItem(track, i)
-            local take = r.GetActiveTake(added_item)
-            local _, take_name = r.GetSetMediaItemTakeInfo_String(take, "P_NAME", "", false)
+            added_item = GetTrackMediaItem(track, i)
+            local take = GetActiveTake(added_item)
+            local _, take_name = GetSetMediaItemTakeInfo_String(take, "P_NAME", "", false)
             if take_name == "" then
-                r.SetMediaItemSelected(added_item, true)
+                SetMediaItemSelected(added_item, true)
                 count = count + 1
             else
                 break
@@ -160,19 +165,20 @@ function select_CD_track_items(item_number, num_of_items, track)
     return count
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function calc_postgap(count, num_of_items, track, selected_item)
-    local last_item_of_track = r.GetSelectedMediaItem(0, 0 + count)
-    local last_item_of_track_pos = r.GetMediaItemInfo_Value(last_item_of_track, "D_POSITION")
-    local last_item_of_track_length = r.GetMediaItemInfo_Value(last_item_of_track, "D_LENGTH")
+    local last_item_of_track = GetSelectedMediaItem(0, 0 + count)
+    local last_item_of_track_pos = GetMediaItemInfo_Value(last_item_of_track, "D_POSITION")
+    local last_item_of_track_length = GetMediaItemInfo_Value(last_item_of_track, "D_LENGTH")
     local last_item_of_track_end = last_item_of_track_pos + last_item_of_track_length
 
     local _, last_item_number = check_position(last_item_of_track)
     local postgap
     local new_track_item
     if last_item_number ~= num_of_items - 1 then
-        new_track_item = r.GetTrackMediaItem(track, last_item_number + 1)
-        local new_track_pos = r.GetMediaItemInfo_Value(new_track_item, "D_POSITION")
+        new_track_item = GetTrackMediaItem(track, last_item_number + 1)
+        local new_track_pos = GetMediaItemInfo_Value(new_track_item, "D_POSITION")
         postgap = new_track_pos - last_item_of_track_end
     else
         new_track_item = selected_item
@@ -181,20 +187,23 @@ function calc_postgap(count, num_of_items, track, selected_item)
     return new_track_item, postgap
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function get_grouped_items(item)
-    r.Main_OnCommand(40289,0) -- unselect all items
-    r.SetMediaItemSelected(item, true)
-    r.Main_OnCommand(40034,0) -- Item grouping: Select all items in groups
-    
-    Selected_item_count = r.CountSelectedMediaItems(0)
-    
+    Main_OnCommand(40289, 0) -- unselect all items
+    SetMediaItemSelected(item, true)
+    Main_OnCommand(40034, 0) -- Item grouping: Select all items in groups
+
+    Selected_item_count = CountSelectedMediaItems(0)
+
     Selected_items = {}
-    
-    for i=1,Selected_item_count - 1 do
-      Selected_items[i] = r.GetSelectedMediaItem(0, i)
+
+    for i = 1, Selected_item_count - 1 do
+        Selected_items[i] = GetSelectedMediaItem(0, i)
     end
     return Selected_items
-   end
----------------------------------------------------------------------------------------
-Main()
+end
+
+---------------------------------------------------------------------
+
+main()
--- ReaClassical/ReaClassical_TrackRight.lua
+++ ReaClassical/ReaClassical_TrackRight.lua
@@ -17,72 +17,78 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 ]]
-local r = reaper
-local takename_check, check_position, get_track_info, select_CD_track_items, last_pos_check, next_track, switch_highlight
----------------------------------------------------------------------------------------
-function Main()
-    r.Undo_BeginBlock()
+
+for key in pairs(reaper) do _G[key] = reaper[key] end
+
+---------------------------------------------------------------------
+
+function main()
+    Undo_BeginBlock()
 
     local take_name, selected_item = takename_check()
     if take_name == -1 or take_name == "" then
-        r.ShowMessageBox('Please select an item that starts a CD track', "Select CD track start", 0)
+        ShowMessageBox('Please select an item that starts a CD track', "Select CD track start", 0)
         return
     end
 
     local first_track, num_of_items, item_number, count = last_pos_check(selected_item)
 
     if item_number + count == num_of_items - 1 then
-        r.Main_OnCommand(40769,0) -- unselect all
-        r.SetMediaItemSelected(selected_item, true)
-        r.ShowMessageBox('The selected track is already in last position', "Select CD track start", 0)
+        Main_OnCommand(40769, 0) -- unselect all
+        SetMediaItemSelected(selected_item, true)
+        ShowMessageBox('The selected track is already in last position', "Select CD track start", 0)
         return
     end
 
     next_track(first_track, item_number, count)
 
-    local ReaClassical_TrackLeft = r.NamedCommandLookup("_RS18fe066cb8806e30b0371fc30a79c67ce2b807f1")
-    r.Main_OnCommand(ReaClassical_TrackLeft,0)
+    local ReaClassical_TrackLeft = NamedCommandLookup("_RS18fe066cb8806e30b0371fc30a79c67ce2b807f1")
+    Main_OnCommand(ReaClassical_TrackLeft, 0)
 
     switch_highlight(selected_item)
 
-    r.Undo_EndBlock("Move Track Right", -1)
+    Undo_EndBlock("Move Track Right", -1)
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function takename_check()
-    local item = r.GetSelectedMediaItem(0, 0)
+    local item = GetSelectedMediaItem(0, 0)
     if item then
-      local take = r.GetActiveTake(item) 
-      local _, take_name = r.GetSetMediaItemTakeInfo_String(take, "P_NAME", "", false)
-      return take_name, item
+        local take = GetActiveTake(item)
+        local _, take_name = GetSetMediaItemTakeInfo_String(take, "P_NAME", "", false)
+        return take_name, item
     else
-      return -1
+        return -1
     end
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function check_position(item)
-    local item_number = r.GetMediaItemInfo_Value(item, "IP_ITEMNUMBER")
+    local item_number = GetMediaItemInfo_Value(item, "IP_ITEMNUMBER")
     return item_number
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function get_track_info()
-    local first_track = r.GetTrack(0, 0)
-    return first_track, r.GetTrackNumMediaItems(first_track)
+    local first_track = GetTrack(0, 0)
+    return first_track, GetTrackNumMediaItems(first_track)
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function select_CD_track_items(item_number, num_of_items, track)
     local count = 0
     local added_item
     if item_number ~= num_of_items - 1 then
         for i = item_number + 1, num_of_items - 1, 1 do
-            added_item = r.GetTrackMediaItem(track, i)
-            local take = r.GetActiveTake(added_item)
-            local _, take_name = r.GetSetMediaItemTakeInfo_String(take, "P_NAME", "", false)
+            added_item = GetTrackMediaItem(track, i)
+            local take = GetActiveTake(added_item)
+            local _, take_name = GetSetMediaItemTakeInfo_String(take, "P_NAME", "", false)
             if take_name == "" then
-                r.SetMediaItemSelected(added_item, true)
+                SetMediaItemSelected(added_item, true)
                 count = count + 1
             else
                 break
@@ -92,18 +98,23 @@ function select_CD_track_items(item_number, num_of_items, track)
     return count
 end
 
----------------------------------------------------------------------------------------
+---------------------------------------------------------------------
+
 function next_track(first_track, item_number, count)
-    r.Main_OnCommand(40769,0) -- unselect all
-    local next_track_item = r.GetTrackMediaItem(first_track, item_number + count + 1)
-    r.SetMediaItemSelected(next_track_item, true)
+    Main_OnCommand(40769, 0) -- unselect all
+    local next_track_item = GetTrackMediaItem(first_track, item_number + count + 1)
+    SetMediaItemSelected(next_track_item, true)
 end
----------------------------------------------------------------------------------------
+
+---------------------------------------------------------------------
+
 function switch_highlight(item)
-    r.Main_OnCommand(40769,0) -- unselect all
-    r.SetMediaItemSelected(item, true)
+    Main_OnCommand(40769, 0) -- unselect all
+    SetMediaItemSelected(item, true)
 end
----------------------------------------------------------------------------------------
+
+---------------------------------------------------------------------
+
 function last_pos_check(item)
     local first_track, num_of_items = get_track_info()
     local item_number = check_position(item)
@@ -111,4 +122,6 @@ function last_pos_check(item)
     return first_track, num_of_items, item_number, count
 end
 
-Main()
+---------------------------------------------------------------------
+
+main()
