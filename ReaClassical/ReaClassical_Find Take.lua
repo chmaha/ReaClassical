68894d37883311fa0547f5869714318ebeb8a5bf
chmaha <reaclassical@gmail.com>
2024-09-18 10:04:38 +0100

    Add input for optional session name

--- ReaClassical/ReaClassical_Find Take.lua
+++ ReaClassical/ReaClassical_Find Take.lua
@@ -25,33 +25,54 @@ local main
 ---------------------------------------------------------------------
 
 function main()
+    local take_choice
+    local _, session_name = GetProjExtState(0, "ReaClassical", "SessionNameSearch")
     ::start::
-    local retval, take_choice = GetUserInputs('Find Take', 1,'Take Number:','')
-    if not retval or take_choice == "" then return end
+    local retval, inputs = GetUserInputs('Find Take', 2, 'Take Number:,Session Name (optional):', ',' .. session_name)
+    if not retval then return end
+
+    take_choice, session_name = inputs:match("([^,]*),([^,]*)")
+    take_choice = tonumber(take_choice)
+    if not take_choice and session_name ~= "" then take_choice = 1 end
+
+    session_name = session_name:match("^%s*(.-)%s*$") -- Trim spaces around the session name
+    SetProjExtState(0, "ReaClassical", "SessionNameSearch", session_name)
+
     local found = false
     local num_of_items = CountMediaItems(0)
-    for i=0, num_of_items -1, 1 do
+
+    for i = 0, num_of_items - 1, 1 do
         local item = GetMediaItem(0, i)
-        local take = reaper.GetActiveTake(item)
-        local src = reaper.GetMediaItemTake_Source(take)
-        local filename = reaper.GetMediaSourceFileName(src, "")
-        local take_capture = tonumber(filename:match(".*[^%d](%d+)%)?%.%a+$"))
-        local edit, _ = GetSetMediaItemInfo_String(item, "P_EXT:SD", "", 0)
-        if take_capture == tonumber(take_choice) and not edit then
-            found = true
-            local item_start = GetMediaItemInfo_Value(item, "D_POSITION")
-            SetEditCurPos(item_start, 1, 0)
-            local center = NamedCommandLookup("_SWS_HSCROLL50")
-            Main_OnCommand(center,0)
-            Main_OnCommand(40769,0) -- unselect all items
-            SetMediaItemSelected(item, true)
-            break
+        local take = GetActiveTake(item)
+        if take then
+            local src = GetMediaItemTake_Source(take)
+            local filename = GetMediaSourceFileName(src, "")
+            local take_capture = tonumber(filename:match(".*[^%d](%d+)%)?%.%a+$"))
+            local session_match = true
+
+            if session_name and session_name ~= "" then
+                session_match = filename:lower():find(session_name:lower()) ~= nil
+            end
+
+            local edit, _ = GetSetMediaItemInfo_String(item, "P_EXT:SD", "", 0)
+
+            if take_capture == take_choice and session_match and not edit then
+                found = true
+                local item_start = GetMediaItemInfo_Value(item, "D_POSITION")
+                SetEditCurPos(item_start, 1, 0)
+                local center = NamedCommandLookup("_SWS_HSCROLL50")
+                Main_OnCommand(center, 0)
+                Main_OnCommand(40769, 0) -- unselect all items
+                SetMediaItemSelected(item, true)
+                break
+            end
         end
     end
-    if not found then
+
+    if not found and (take_choice or session_name ~= "") then
         local response = ShowMessageBox("Take not found. Try again?", "Find Take", 4)
         if response == 6 then
-        goto start
+            goto start
         end
     end
 end
