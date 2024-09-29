87d26303a34d702dfaff44848824a2b45116804c
chmaha <reaclassical@gmail.com>
2024-09-29 12:47:08 +0100

    24.22.15

--- ReaClassical/ReaClassical.lua
+++ ReaClassical/ReaClassical.lua
@@ -1,11 +1,8 @@
 @description ReaClassical
 @author chmaha
-@version 24.22.14
+@version 24.22.15
 @changelog
-  Assembly Line Edit: Add threshold for when dest-in marker is placed inside series of items
-  Q/W in arrange window: Q moves directly to previous item independent of cursor pos
-  Crossfade Editor and Q/W: Only open or move to crossfaded pairs
-  Create CD Markers: Add clarity to message when no item names
+  Crossfade Editor: When using Q/W to cycle, show message if user reaches first or final xfade pair
 @metapackage
 @provides
   [main] ReaClassical_Add Destination IN marker.lua
