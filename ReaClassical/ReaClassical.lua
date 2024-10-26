98c8e157f6e84a0c64acf48da337e306a324e2c1
chmaha <reaclassical@gmail.com>
2024-10-26 19:15:07 +0100

    Use correct number option in RCprefs

--- ReaClassical/ReaClassical.lua
+++ ReaClassical/ReaClassical.lua
@@ -1,8 +1,8 @@
 @description ReaClassical
 @author chmaha
-@version 24.25.12
+@version 24.25.13
 @changelog
-  ReaClassical Preferences: Fix numbering since removing one of the options
+  Assembly Edit: Use correct numbered entry in RC Preferences
 @metapackage
 @provides
   [main] ReaClassical_Add Destination IN marker.lua
