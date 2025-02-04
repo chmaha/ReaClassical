626a681e453d422c8286ef3da52266837d691b9e
chmaha <reaclassical@gmail.com>
2025-02-04 13:00:27 +0000

    25.2.3

--- ReaClassical/ReaClassical.lua
+++ ReaClassical/ReaClassical.lua
@@ -1,8 +1,8 @@
 @description ReaClassical
 @author chmaha
-@version 25.2.2
+@version 25.2.3
 @changelog
-  S-D editing: Auto disable trim content behind media items
+  Assembly Editing: Only split at destination IN marker
 @metapackage
 @provides
   [main] ReaClassical_Add Destination IN marker.lua
