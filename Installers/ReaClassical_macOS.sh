4eb7799d4c5835f1f744d8ff0cbfccd0655795db
chmaha <reaclassical@gmail.com>
2025-02-05 19:50:43 +0000

    Add GPLv3 text

--- Installers/ReaClassical_macOS.sh
+++ Installers/ReaClassical_macOS.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
-# by chmaha (April 2024)
 
 # Script to install ReaClassical on MacOS
 # Works for all architectures and OS versions that are compatible with REAPER
+
+# Copyright (C) 2022–2025 chmaha
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 cleanup() {
     if [ -n "$temp_dir" ] && [ -d "$temp_dir" ]; then
