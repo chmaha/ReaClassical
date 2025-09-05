3454291946a183b3e0cdbce2dfcf2d613f707bdc
chmaha <reaclassical@gmail.com>
2025-09-05 09:45:46 -0400

    Use portable source until release

--- scripts/copy_over_lua.sh
+++ scripts/copy_over_lua.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # copy over changed lua files from REAPER scripts folder
 
-SRC="$HOME/.config/REAPER/Scripts/chmaha Scripts/ReaClassical"
+SRC="$HOME/Desktop/ReaClassical_26/Scripts/chmaha Scripts/ReaClassical" -- change back to SRC="$HOME/.config/REAPER/Scripts/chmaha Scripts/ReaClassical" later!
 DEST="ReaClassical"
 
 cp "$SRC"/*.lua "$DEST"
