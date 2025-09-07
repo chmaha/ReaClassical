8537643303a634e5ec7911c944479c0df1deebb0
chmaha <reaclassical@gmail.com>
2025-09-07 12:31:23 -0400

    Fix cursorup and cursordown in PDF manual

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -4,4 +4,4 @@ current_date=$(TZ=UTC date "+%Y-%m-%d %H:%M:%S")
 sed -i "s/\`Updated:.*/\`Updated: $current_date UTC\`/" "$front_matter" \
 && sed -i "s/^Generated.*/Generated $current_date UTC/" "$index" \
 && asciidoctor -o docs/manual/index.html docs/manual/src/index.adoc \
-&& asciidoctor-pdf -a imagesdir=assets docs/manual/src/index.adoc -o PDF-Manual/ReaClassical-Manual.pdf
+&& asciidoctor-pdf --theme default-with-font-fallbacks -a imagesdir=assets docs/manual/src/index.adoc -o PDF-Manual/ReaClassical-Manual.pdf
