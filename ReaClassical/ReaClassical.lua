6f9373e4a8f7e63cb275a69d61622901377bf85c
chmaha <reaclassical@gmail.com>
2025-10-29 13:51:37 -0400

    Add ReaClassical.lua back to retain compatibility for RC25

---
+++ ReaClassical/ReaClassical.lua
@@ -0,0 +1,127 @@
+@description ReaClassical
+@author chmaha
+@version 25.10
+@changelog
+  Documentation: Modify REAPER update instructions
+@metapackage
+@provides
+  [main] ReaClassical_Add Destination IN marker.lua
+  [main] ReaClassical_Add Destination OUT Marker.lua
+  [main] ReaClassical_Add Source IN marker.lua
+  [main] ReaClassical_Add Source OUT marker.lua
+  [main] ReaClassical_Classical Crossfade Editor.lua
+  [main] ReaClassical_Classical Crossfade.lua
+  [main] ReaClassical_Classical Take Record.lua
+  [main] ReaClassical_Delete All S-D markers.lua
+  [main] ReaClassical_Duplicate folder (No items).lua
+  [main] ReaClassical_Prepare Takes.lua
+  [main] ReaClassical_S-D Edit.lua
+  [main] ReaClassical_Whole Project View Horizontal.lua
+  [main] ReaClassical_Whole Project View Vertical.lua
+  [main] ReaClassical_Vertical Workflow.lua
+  [main] ReaClassical_Audition.lua
+  [main] ReaClassical_Delete Leaving Silence.lua
+  [main] ReaClassical_Delete With Ripple.lua
+  [main] ReaClassical_Insert with timestretching.lua
+  [main] ReaClassical_Horizontal Workflow.lua
+  [main] ReaClassical_Lock_toggle.lua
+  [main] ReaClassical_Create CD Markers.lua
+  [main] ReaClassical_Help.lua
+  [main] ReaClassical_Next Item or Fade.lua
+  [main] ReaClassical_Previous Item or Fade.lua
+  [main] ReaClassical_Add Aux.lua
+  [main] ReaClassical_Add Submix.lua
+  [main] ReaClassical_Reposition_Album_Tracks.lua
+  [main] ReaClassical_Preferences.lua
+  [main] ReaClassical_TrackLeft.lua
+  [main] ReaClassical_TrackRight.lua
+  [main] ReaClassical_ExplodeMultiChannel.lua
+  [main] ReaClassical_Colorize.lua
+  [main] ReaClassical_Add RoomTone Track.lua
+  [main] ReaClassical_Move to Destination IN marker.lua
+  [main] ReaClassical_Move to Destination OUT marker.lua
+  [main] ReaClassical_Move to Source IN marker.lua
+  [main] ReaClassical_Move to Source OUT marker.lua
+  [main] ReaClassical_Zoom to Destination IN marker.lua
+  [main] ReaClassical_Zoom to Destination OUT marker.lua
+  [main] ReaClassical_Zoom to Source IN marker.lua
+  [main] ReaClassical_Zoom to Source OUT marker.lua
+  [main] ReaClassical_3-point Insert Edit.lua
+  [main] ReaClassical_Updater.lua
+  [main] ReaClassical_Find Take.lua
+  [main] ReaClassical_Take Counter.lua
+  [main] ReaClassical_Add Track To All Groups.lua
+  [main] ReaClassical_Hide Children.lua
+  [main] ReaClassical_Show Children.lua
+  [main] ReaClassical_Automation Mode.lua
+  [main] ReaClassical_Insert Automation.lua
+  [main] ReaClassical_Set Dest Project Marker.lua
+  [main] ReaClassical_Set Source Project Marker.lua
+  [main] ReaClassical_Delete Track From All Groups.lua
+  [main] ReaClassical_Add Ref Track.lua
+  [main] ReaClassical_Source Markers to Item Edge.lua
+  [main] ReaClassical_Destination Markers to Item Edge.lua
+  [main] ReaClassical_Remove Take Names.lua
+  [main] ReaClassical_Jump To Time.lua
+  [main] ReaClassical_Audio Calculator.lua
+  [main] ReaClassical_Auto Set Recording Inputs.lua
+  [main] ReaClassical_Mastering Mode.lua
+  [main] ReaClassical_Audition_with_playrate.lua
+  [main] ReaClassical_Increment Take Number While Recording.lua
+  [main] ReaClassical_Rank Higher.lua
+  [main] ReaClassical_Rank Lower.lua
+  [main] ReaClassical_Remove Ranking.lua
+  [main] ReaClassical_Add Special Tracks.lua
+  [main] ReaClassical_Delete S-D Project Markers.lua
+  [main] ReaClassical_Copy Destination Material to Source.lua
+  [main] ReaClassical_Move Destination Material to Source.lua
+  [main] ReaClassical_Show Statistics.lua
+  [main] ReaClassical_Reverse S-D Edit.lua
+  [main] ReaClassical_Build Edit List.lua
+  [main] ReaClassical_Build Edit List using BWF offset.lua
+  [main] ReaClassical_Metadata Report.lua
+  [main] ReaClassical_Convert REAPER project.lua
+  [main] ReaClassical_Split Items at Markers.lua
+  [main] ReaClassical_Zoom to All Items Toggle.lua
+  [main] ReaClassical_Set Next Recording Section.lua
+  [main] ReaClassical_Update REAPER.lua
+  [nomain] ReaClassical_Play Both Items of Crossfade.lua
+  [nomain] ReaClassical_Play Both Items of Crossfade with playrate.lua
+  [nomain] ReaClassical_Play Left Crossfade Item.lua
+  [nomain] ReaClassical_Play Left Crossfade Item with playrate.lua
+  [nomain] ReaClassical_Play Right Crossfade Item.lua
+  [nomain] ReaClassical_Play Right Crossfade Item with playrate.lua
+  [rpp] ReaClassical.RPP
+  [rpp] Room_Tone_Generation.RPP
+  [theme] ReaClassical.ReaperThemeZip
+  [theme] ReaClassical Light.ReaperThemeZip
+  [theme] ReaClassical WaveColors Dark.ReaperThemeZip
+  [theme] ReaClassical WaveColors Light.ReaperThemeZip
+  [data] copy_dest_material.png > toolbar_icons/copy_dest_material.png
+  [data] move_dest_material.png > toolbar_icons/move_dest_material.png
+  [data] delete leaving silence.png > toolbar_icons/delete leaving silence.png
+  [data] Delete SD Markers.png > toolbar_icons/Delete SD Markers.png
+  [data] Delete SD Project Markers.png > toolbar_icons/Delete SD Project Markers.png
+  [data] delete with ripple.png > toolbar_icons/delete with ripple.png
+  [data] Dest IN.png > toolbar_icons/Dest IN.png
+  [data] Dest OUT.png > toolbar_icons/Dest OUT.png
+  [data] Insert with timestretching.png > toolbar_icons/Insert with timestretching.png
+  [data] SD Edit.png > toolbar_icons/SD Edit.png
+  [data] Set_Dest_Proj.png > toolbar_icons/Set_Dest_Proj.png
+  [data] Set_Source_Proj.png > toolbar_icons/Set_Source_Proj.png
+  [data] source IN.png > toolbar_icons/source IN.png
+  [data] source OUT.png > toolbar_icons/source OUT.png
+  [data] Reverse SD Edit.png > toolbar_icons/Reverse SD Edit.png
+  [data] assembly.png > toolbar_icons/assembly.png
+  ReaClassical_Colors_Table.lua > ReaClassical_Colors_Table.lua
+  ../PDF-Manual/ReaClassical-Manual.pdf > ReaClassical-Manual.pdf
+  reaclassical-splash.png > reaclassical-splash.png
+  ReaClassical-menu.ini > ReaClassical-menu.ini
+  ReaClassical-kb.ini > ReaClassical-kb.ini
+  ReaClassical-render.ini > ReaClassical-render.ini
+  ../audio_calculator.html > audio_calculator.html
+  ../docs/images/reaclassical_os.png > reaclassical_logo.png
+  ../tested_reaper_ver.txt > tested_reaper_ver.txt
+@about
+  These scripts, along with the included custom project template and theme, provide everything you need for professional classical music editing, mixing and mastering in REAPER.
+
