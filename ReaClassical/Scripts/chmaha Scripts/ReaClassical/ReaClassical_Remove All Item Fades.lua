68505e8f8cc4387a76ef491963724d4d2c1ec18a
chmaha <reaclassical@gmail.com>
2025-12-11 07:59:46 -0500

    NO-OP: rearrange functions

--- ReaClassical/Scripts/chmaha Scripts/ReaClassical/ReaClassical_Remove All Item Fades.lua
+++ ReaClassical/Scripts/chmaha Scripts/ReaClassical/ReaClassical_Remove All Item Fades.lua
@@ -25,38 +25,6 @@ local main, item_edge_overlaps
 
 ---------------------------------------------------------------------
 
--- Helper: check if an item's start or end overlaps with another item on the same track
-function item_edge_overlaps(item, check_start)
-  local track = GetMediaItem_Track(item)
-  local item_start = GetMediaItemInfo_Value(item, "D_POSITION")
-  local item_len = GetMediaItemInfo_Value(item, "D_LENGTH")
-  local item_end = item_start + item_len
-  local item_count = CountTrackMediaItems(track)
-
-  for j = 0, item_count - 1 do
-    local other = GetTrackMediaItem(track, j)
-    if other ~= item then
-      local other_start = GetMediaItemInfo_Value(other, "D_POSITION")
-      local other_len = GetMediaItemInfo_Value(other, "D_LENGTH")
-      local other_end = other_start + other_len
-
-      -- Check overlap at start or end
-      if check_start then
-        if other_start < item_start and other_end > item_start then
-          return true -- overlap at start edge
-        end
-      else
-        if other_start < item_end and other_end > item_end then
-          return true -- overlap at end edge
-        end
-      end
-    end
-  end
-  return false
-end
-
----------------------------------------------------------------------
-
 function main()
   PreventUIRefresh(1)
   Undo_BeginBlock()
@@ -90,6 +58,37 @@ function main()
   PreventUIRefresh(-1)
   UpdateArrange()
   UpdateTimeline()
+end
+
+---------------------------------------------------------------------
+
+function item_edge_overlaps(item, check_start)
+  local track = GetMediaItem_Track(item)
+  local item_start = GetMediaItemInfo_Value(item, "D_POSITION")
+  local item_len = GetMediaItemInfo_Value(item, "D_LENGTH")
+  local item_end = item_start + item_len
+  local item_count = CountTrackMediaItems(track)
+
+  for j = 0, item_count - 1 do
+    local other = GetTrackMediaItem(track, j)
+    if other ~= item then
+      local other_start = GetMediaItemInfo_Value(other, "D_POSITION")
+      local other_len = GetMediaItemInfo_Value(other, "D_LENGTH")
+      local other_end = other_start + other_len
+
+      -- Check overlap at start or end
+      if check_start then
+        if other_start < item_start and other_end > item_start then
+          return true -- overlap at start edge
+        end
+      else
+        if other_start < item_end and other_end > item_end then
+          return true -- overlap at end edge
+        end
+      end
+    end
+  end
+  return false
 end
 
 ---------------------------------------------------------------------
