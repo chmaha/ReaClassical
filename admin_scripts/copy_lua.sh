7634a3f9ac11019d7f4c723c644e92b3e30dc087
chmaha <reaclassical@gmail.com>
2026-01-01 14:17:32 -0500

    Tweak lua copy

--- admin_scripts/copy_lua.sh
+++ admin_scripts/copy_lua.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # copy over changed lua files from REAPER scripts folder
-
 SRC="$HOME/Desktop/ReaClassical_26"
 DEST="$HOME/code/chmaha/ReaClassical/ReaClassical"
 
-cp "$SRC/Scripts/chmaha Scripts/ReaClassical/"* "$DEST"
+rsync -av --delete --exclude='ReaClassical.lua' "$SRC/Scripts/chmaha Scripts/ReaClassical/" "$DEST/"
 cp "$SRC/ColorThemes/"* "$DEST"
 cp "$SRC/ProjectTemplates/ReaClassical.RPP" "$DEST"
 cp "$SRC/ProjectTemplates/Room_Tone_Gen.RPP" "$DEST"
