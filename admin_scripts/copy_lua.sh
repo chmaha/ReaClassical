a1c1e18e3f72aa03b7d0842be067062dd826ada8
chmaha <reaclassical@gmail.com>
2026-01-18 09:45:05 -0500

    Add ini files as part of copy

--- admin_scripts/copy_lua.sh
+++ admin_scripts/copy_lua.sh
@@ -7,3 +7,11 @@ rsync -av --delete --exclude='ReaClassical.lua' "$SRC/Scripts/chmaha Scripts/Rea
 cp "$SRC/ColorThemes/"* "$DEST"
 cp "$SRC/ProjectTemplates/ReaClassical.RPP" "$DEST"
 cp "$SRC/ProjectTemplates/Room_Tone_Gen.RPP" "$DEST"
+
+cp $SRC/reaper.ini "$DEST/ReaClassical.ini"
+cp $SRC/reaper-kb.ini "$DEST/ReaClassical-kb.ini"
+cp $SRC/reaper-mouse.ini "$DEST/ReaClassical-mouse.ini"
+cp $SRC/reaper-menu.ini "$DEST/ReaClassical-menu.ini"
+cp $SRC/reaper-render.ini "$DEST/ReaClassical-render.ini"
+
+
