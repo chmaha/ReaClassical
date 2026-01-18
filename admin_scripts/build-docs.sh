87c1cf996668d9404e2394911a597b1e675b5250
chmaha <reaclassical@gmail.com>
2026-01-18 09:19:06 -0500

    Add updates to Mission Control and Metadata Editor

--- admin_scripts/build-docs.sh
+++ admin_scripts/build-docs.sh
@@ -1,4 +1,5 @@
 RC_install="$HOME/Desktop/ReaClassical_26/Scripts/chmaha Scripts/ReaClassical/ReaClassical-Manual.pdf"
+REAPACK="$HOME/code/chmaha/ReaClassical/ReaClassical/ReaClassical-Manual.pdf"
 front_matter="docs/manual/src/front_matter/front_matter.adoc"
 index="docs/manual/src/index.adoc"
 current_date=$(TZ=UTC date "+%Y-%m-%d %H:%M:%S")
@@ -6,4 +7,6 @@ sed -i "s/\`Updated:.*/\`Updated: $current_date UTC\`/" "$front_matter" \
 && sed -i "s/^Generated.*/Generated $current_date UTC/" "$index" \
 && asciidoctor -o docs/manual/index.html docs/manual/src/index.adoc \
 && asciidoctor-pdf -a imagesdir=assets docs/manual/src/index.adoc -o PDF-Manual/ReaClassical-Manual.pdf \
-&& cp PDF-Manual/ReaClassical-Manual.pdf "$RC_install"
+&& cp PDF-Manual/ReaClassical-Manual.pdf "$RC_install" \
+&& cp PDF-Manual/ReaClassical-Manual.pdf "$REAPACK"
+
