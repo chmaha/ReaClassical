b483f343db70ffd40891cdc435abe2a3105e0b0e
chmaha <reaclassical@gmail.com>
2026-04-25 17:45:29 -0400

    Exclusive select parent track associated with S-D marker

--- ReaClassical/ReaClassical_Move to Destination OUT marker.lua
+++ ReaClassical/ReaClassical_Move to Destination OUT marker.lua
@@ -22,7 +22,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 for key in pairs(reaper) do _G[key] = reaper[key] end
 
-local main, markers, move_to_project_tab
+local main, markers, exclusive_select_folder_parent
 
 ---------------------------------------------------------------------
 
@@ -38,10 +38,15 @@ function main()
             .. "+N to use this function.", "ReaClassical Error", 0)
         return
     end
-    local _, _, dest_proj = markers()
 
-    if dest_proj then
-        move_to_project_tab(dest_proj)
+    local marker_count, folder_prefix = markers()
+
+    if marker_count == 0 then return end
+
+    if workflow == "horizontal" then
+        exclusive_select_folder_parent(nil)
+    else
+        exclusive_select_folder_parent(folder_prefix)
     end
 
     GoToMarker(0, 997, false)
@@ -50,94 +55,64 @@ end
 ---------------------------------------------------------------------
 
 function markers()
-    local marker_labels = { "DEST-IN", "DEST-OUT", "SOURCE-IN", "SOURCE-OUT" }
-    local sd_markers = {}
-    for _, label in ipairs(marker_labels) do
-        sd_markers[label] = { count = 0, proj = nil }
-    end
-
+    local marker_count = 0
+    local folder_prefix = nil
     local num = 0
-    local source_proj, dest_proj
-    local proj_marker_count = 0
-    local pos_table = {}
-    local active_proj = EnumProjects(-1)
-    local track_number = 1
+
     while true do
         local proj = EnumProjects(num)
-        if proj == nil then
-            break
-        end
+        if proj == nil then break end
         local _, num_markers, num_regions = CountProjectMarkers(proj)
-        for i = 0, num_markers + num_regions - 1, 1 do
-            local _, _, pos, _, raw_label, _ = EnumProjectMarkers2(proj, i)
-            local label = string.match(raw_label, ".+:(.+)") or ""
-
-            if label == "DEST-IN" then
-                sd_markers[label].count = 1
-                sd_markers[label].proj = proj
-                pos_table[1] = pos
-            elseif label == "DEST-OUT" then
-                sd_markers[label].count = 1
-                sd_markers[label].proj = proj
-                pos_table[2] = pos
-            elseif label == "SOURCE-IN" then
-                sd_markers[label].count = 1
-                sd_markers[label].proj = proj
-                pos_table[3] = pos
-            elseif label == "SOURCE-OUT" then
-                sd_markers[label].count = 1
-                sd_markers[label].proj = proj
-                pos_table[4] = pos
-            elseif string.match(label, "SOURCE PROJECT") then
-                source_proj = proj
-                proj_marker_count = proj_marker_count + 1
-            elseif string.match(label, "DEST PROJECT") then
-                dest_proj = proj
-                proj_marker_count = proj_marker_count + 1
+        for i = 0, num_markers + num_regions - 1 do
+            local _, _, _, _, raw_label, _ = EnumProjectMarkers2(proj, i)
+            local prefix = string.match(raw_label, "^(.+):DEST%-OUT$")
+            if prefix then
+                marker_count = 1
+                folder_prefix = prefix ~= "" and prefix or nil
+            elseif string.match(raw_label, "^DEST%-OUT$") then
+                marker_count = 1
             end
         end
         num = num + 1
     end
 
-    if proj_marker_count == 0 then
-        for _, marker in pairs(sd_markers) do
-            if marker.proj ~= active_proj then
-                marker.count = 0
-            end
-        end
-    end
-
-    local source_in = sd_markers["SOURCE-IN"].count
-    local source_out = sd_markers["SOURCE-OUT"].count
-    local dest_in = sd_markers["DEST-IN"].count
-    local dest_out = sd_markers["DEST-OUT"].count
-
-    local sin = sd_markers["SOURCE-IN"].proj
-    local sout = sd_markers["SOURCE-OUT"].proj
-    local din = sd_markers["DEST-IN"].proj
-    local dout = sd_markers["DEST-OUT"].proj
-
-
-    local source_count = source_in + source_out
-    local dest_count = dest_in + dest_out
-
-    if (source_count == 2 and sin ~= sout) or (dest_count == 2 and din ~= dout) then proj_marker_count = -1 end
-
-    if source_proj and ((sin and sin ~= source_proj) or (sout and sout ~= source_proj)) then
-        proj_marker_count = -1
-    end
-    if dest_proj and ((din and din ~= dest_proj) or (dout and dout ~= dest_proj)) then
-        proj_marker_count = -1
-    end
-
-    return proj_marker_count, source_proj, dest_proj, dest_in, dest_out, dest_count,
-        source_in, source_out, source_count, pos_table, track_number
+    return marker_count, folder_prefix
 end
 
 ---------------------------------------------------------------------
 
-function move_to_project_tab(proj_type)
-    SelectProjectInstance(proj_type)
+function exclusive_select_folder_parent(prefix)
+    local num_tracks = CountTracks(0)
+
+    -- Deselect all tracks first
+    for i = 0, num_tracks - 1 do
+        local track = GetTrack(0, i)
+        SetTrackSelected(track, false)
+    end
+
+    local target_track = nil
+
+    if prefix == nil then
+        -- Horizontal workflow: always select track 1 (index 0)
+        if num_tracks > 0 then
+            target_track = GetTrack(0, 0)
+        end
+    else
+        -- Vertical workflow: find the folder parent whose name starts with the prefix
+        for i = 0, num_tracks - 1 do
+            local track = GetTrack(0, i)
+            local _, track_name = GetTrackName(track)
+            local depth = GetMediaTrackInfo_Value(track, "I_FOLDERDEPTH")
+            if depth == 1 and string.match(track_name, "^" .. prefix) then
+                target_track = track
+                break
+            end
+        end
+    end
+
+    if target_track then
+        SetTrackSelected(target_track, true)
+    end
 end
 
 ---------------------------------------------------------------------
--- ReaClassical/ReaClassical_Zoom to Source OUT marker.lua
+++ ReaClassical/ReaClassical_Zoom to Source OUT marker.lua
@@ -22,7 +22,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 for key in pairs(reaper) do _G[key] = reaper[key] end
 
-local main, markers, move_to_project_tab, zoom
+local main, markers, exclusive_select_folder_parent, zoom
 
 ---------------------------------------------------------------------
 
@@ -38,41 +38,85 @@ function main()
             .. "+N to use this function.", "ReaClassical Error", 0)
         return
     end
-    -- Check if marker exists in any project
-    local marker_exists = false
-    local i = 0
-    while true do
-        local proj = EnumProjects(i)
-        if proj == nil then break end
 
+    local marker_count, folder_prefix = markers()
+
+    if marker_count == 0 then return end
+
+    if workflow == "horizontal" then
+        exclusive_select_folder_parent(nil)
+    else
+        exclusive_select_folder_parent(folder_prefix)
+    end
+
+    GoToMarker(0, 999, false)
+    zoom()
+end
+
+---------------------------------------------------------------------
+
+function markers()
+    local marker_count = 0
+    local folder_prefix = nil
+    local num = 0
+
+    while true do
+        local proj = EnumProjects(num)
+        if proj == nil then break end
         local _, num_markers, num_regions = CountProjectMarkers(proj)
-        for j = 0, num_markers + num_regions - 1 do
-            local _, region, _, _, _, id = EnumProjectMarkers2(proj, j)
-            if not region and id == 999 then
-                marker_exists = true
+        for i = 0, num_markers + num_regions - 1 do
+            local _, _, _, _, raw_label, _ = EnumProjectMarkers2(proj, i)
+            local prefix = string.match(raw_label, "^(.+):SOURCE%-OUT$")
+            if prefix then
+                marker_count = 1
+                folder_prefix = prefix ~= "" and prefix or nil
+            elseif string.match(raw_label, "^SOURCE%-OUT$") then
+                marker_count = 1
+            end
+        end
+        num = num + 1
+    end
+
+    return marker_count, folder_prefix
+end
+
+---------------------------------------------------------------------
+
+function exclusive_select_folder_parent(prefix)
+    local num_tracks = CountTracks(0)
+
+    for i = 0, num_tracks - 1 do
+        local track = GetTrack(0, i)
+        SetTrackSelected(track, false)
+    end
+
+    local target_track = nil
+
+    if prefix == nil then
+        if num_tracks > 0 then
+            target_track = GetTrack(0, 0)
+        end
+    else
+        for i = 0, num_tracks - 1 do
+            local track = GetTrack(0, i)
+            local _, track_name = GetTrackName(track)
+            local depth = GetMediaTrackInfo_Value(track, "I_FOLDERDEPTH")
+            if depth == 1 and string.match(track_name, "^" .. prefix) then
+                target_track = track
                 break
             end
         end
-
-        if marker_exists then break end
-        i = i + 1
     end
-    if marker_exists then
-        local _, _, dest_proj = markers()
 
-        if dest_proj then
-            move_to_project_tab(dest_proj)
-        end
-
-        GoToMarker(0, 999, false)
-        zoom()
+    if target_track then
+        SetTrackSelected(target_track, true)
     end
 end
 
 ---------------------------------------------------------------------
 
 function zoom()
-    local cur_pos  = (GetPlayState() == 0)
+    local cur_pos = (GetPlayState() == 0)
         and GetCursorPosition()
         or GetPlayPosition()
 
@@ -87,99 +131,6 @@ function zoom()
     SetEditCurPos(cur_pos, false, false)
     Main_OnCommand(1012, 0)  -- View: Zoom in horizontal
     Main_OnCommand(40635, 0) -- Time selection: Remove
-end
-
----------------------------------------------------------------------
-
-function markers()
-    local marker_labels = { "DEST-IN", "DEST-OUT", "SOURCE-IN", "SOURCE-OUT" }
-    local sd_markers = {}
-    for _, label in ipairs(marker_labels) do
-        sd_markers[label] = { count = 0, proj = nil }
-    end
-
-    local num = 0
-    local source_proj, dest_proj
-    local proj_marker_count = 0
-    local pos_table = {}
-    local active_proj = EnumProjects(-1)
-    local track_number = 1
-    while true do
-        local proj = EnumProjects(num)
-        if proj == nil then
-            break
-        end
-        local _, num_markers, num_regions = CountProjectMarkers(proj)
-        for i = 0, num_markers + num_regions - 1, 1 do
-            local _, _, pos, _, raw_label, _ = EnumProjectMarkers2(proj, i)
-            local label = string.match(raw_label, ".+:(.+)") or ""
-
-            if label == "DEST-IN" then
-                sd_markers[label].count = 1
-                sd_markers[label].proj = proj
-                pos_table[1] = pos
-            elseif label == "DEST-OUT" then
-                sd_markers[label].count = 1
-                sd_markers[label].proj = proj
-                pos_table[2] = pos
-            elseif label == "SOURCE-IN" then
-                sd_markers[label].count = 1
-                sd_markers[label].proj = proj
-                pos_table[3] = pos
-            elseif label == "SOURCE-OUT" then
-                sd_markers[label].count = 1
-                sd_markers[label].proj = proj
-                pos_table[4] = pos
-            elseif string.match(label, "SOURCE PROJECT") then
-                source_proj = proj
-                proj_marker_count = proj_marker_count + 1
-            elseif string.match(label, "DEST PROJECT") then
-                dest_proj = proj
-                proj_marker_count = proj_marker_count + 1
-            end
-        end
-        num = num + 1
-    end
-
-    if proj_marker_count == 0 then
-        for _, marker in pairs(sd_markers) do
-            if marker.proj ~= active_proj then
-                marker.count = 0
-            end
-        end
-    end
-
-    local source_in = sd_markers["SOURCE-IN"].count
-    local source_out = sd_markers["SOURCE-OUT"].count
-    local dest_in = sd_markers["DEST-IN"].count
-    local dest_out = sd_markers["DEST-OUT"].count
-
-    local sin = sd_markers["SOURCE-IN"].proj
-    local sout = sd_markers["SOURCE-OUT"].proj
-    local din = sd_markers["DEST-IN"].proj
-    local dout = sd_markers["DEST-OUT"].proj
-
-
-    local source_count = source_in + source_out
-    local dest_count = dest_in + dest_out
-
-    if (source_count == 2 and sin ~= sout) or (dest_count == 2 and din ~= dout) then proj_marker_count = -1 end
-
-    if source_proj and ((sin and sin ~= source_proj) or (sout and sout ~= source_proj)) then
-        proj_marker_count = -1
-    end
-    if dest_proj and ((din and din ~= dest_proj) or (dout and dout ~= dest_proj)) then
-        proj_marker_count = -1
-    end
-
-    return proj_marker_count, source_proj, dest_proj, dest_in, dest_out, dest_count,
-        source_in, source_out, source_count, pos_table, track_number
-end
-
----------------------------------------------------------------------
-
-function move_to_project_tab(proj_type)
-    SelectProjectInstance(proj_type)
 end
 
 ---------------------------------------------------------------------
