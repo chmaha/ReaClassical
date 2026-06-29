75b902e74702041ca05d168da464ac6f6c7663ee
chmaha <reaclassical@gmail.com>
2026-06-29 11:17:18 -0400

    26.7pre4

--- ReaClassical/ReaClassical.lua
+++ ReaClassical/ReaClassical.lua
@@ -1,6 +1,6 @@
 @description ReaClassical
 @author chmaha
-@version 26.7pre3
+@version 26.7pre4
 @changelog
   NEW: accessibility layer
   NEW: Terminal commands including complete domain-specific language
